0dd073b43fdce1aa97b18ea0bc7256e46e304257
Daniel López <d.lopez.lopez@udc.es>
2018-12-05 15:45:17 +0100

    Inicial.

---
+++ Practica 2/practica2.ml
@@ -0,0 +1,15 @@
+(*
+@author Daniel López López
+*)
+
+#load "talf.cma";;
+open Conj;;
+open Auto;;
+open Ergo;;
+open Graf;;
+
+let ap_of_gic Gic( no_terminales, terminales, reglas, s) =
+  let rec aux =
+
+  in aux 
+;;
